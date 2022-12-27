253e13b0554c12f73b343456f972bce199482c26
Mark Witt <mwitt@newstore.com>
2022-12-27 17:19:16 +0100

    Version 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # OneLine Changelog
 
+## [Version 0.0.1] - 2022-12-27
 ## [Initial Version] - 2022-12-27
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
-import { Detail } from "@raycast/api";
+import { Action, ActionPanel, Form, popToRoot } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  const [value, setValue] = useCachedState("input", "");
+  const output = value.replaceAll(/\r?\n|\r/g, " ");
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.CopyToClipboard content={output} title="Copy to Clipboard" />
+        </ActionPanel>
+      }
+    >
+      <Form.TextArea
+        id="input"
+        title="Input"
+        placeholder="Enter text you want to convert to one line..."
+        onChange={setValue}
+      />
+      <Form.TextArea id="output" value={output} />
+    </Form>
+  );
 }
